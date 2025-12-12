83228e2fec46c13e15757d19594c61701eda719d
advitabathole <advitabathole@gmail.com>
2025-12-12 14:15:42 -0800

    Add files via upload

---
+++ PA0/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        //Put your code below
+        System.out.println("My name is Advita Bathole");
+        System.out.println("My favorite hobby is reading");
+    }
+}
